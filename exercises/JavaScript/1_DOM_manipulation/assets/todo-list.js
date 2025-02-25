54b64d9785cf2639bf50f2f7d43bc602ce1592cb
AlexMenezes <alexmenezes141184@gmail.com>
2025-02-25 09:57:56 +0000

    Final commit

--- exercises/JavaScript/1_DOM_manipulation/assets/todo-list.js
+++ exercises/JavaScript/1_DOM_manipulation/assets/todo-list.js
@@ -10,6 +10,9 @@ input.ariaLabel = "new item";
 
 button.type = "submit";
 button.textContent = "add";
+button.classList.add('highlight');
+
+addForm.classList.add('with-button');
 
 addForm.append(input, button);
 
@@ -17,9 +20,26 @@ main.append(addForm, list);
 
 addForm.addEventListener('submit', ev => {
     ev.preventDefault();
-    if(!input.value) return;
+    if (!input.value) return;
     const li = document.createElement('li');
-    li.textContent = input.value;
+    const label = document.createElement('label');
+    const done = document.createElement('input');
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerHTML = "&times;";
+    deleteBtn.classList.add('highlight');
+    label.textContent = input.value;
+    label.classList.add('item');
+    done.type = "checkbox";
+    label.append(done);
+    li.classList.add('with-button');
+    li.append(label, deleteBtn);
     input.value = "";
     list.append(li);
+
+    deleteBtn.addEventListener('click', ev => {
+        li.classList.add('removing');
+        li.addEventListener('animationend', ev => {
+            li.remove();
+        })
+    });
 });
