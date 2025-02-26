d1561a1c1ed3a9f48307bfce17298aaa61cf602f
--help <alexmenezes141184@gmail.com>
2025-02-26 14:26:20 +0000

    Completed class material

--- exercises/JavaScript/HTML_Canvas/assets/canvas.js
+++ exercises/JavaScript/HTML_Canvas/assets/canvas.js
@@ -188,6 +188,52 @@
 
 // Adding structure
 
+// import face from './face.js'
+
+// const w = window.innerWidth;
+// const h = window.innerHeight;
+
+// const canvas = document.getElementById('canvas');
+
+// canvas.width = w;
+// canvas.height = h;
+
+// const ctx = canvas.getContext('2d');
+
+// function flower(ctx, radius) {
+//     const r = radius / 4;
+
+//     // One in the middle
+//     face(ctx, r);
+    
+//     // eight around the outside
+//     for (const angle of [0, 1, 2, 3, 4, 5, 6, 7]) {
+//         ctx.save();
+//         ctx.rotate(angle * Math.PI / 4);
+//         ctx.translate(0, -r * 1.5);
+//         face(ctx, r / 2);
+//         ctx.restore()    
+//     }
+// }
+
+// function randomFlower(ctx) {
+//     ctx.save();
+//     const x = w * Math.random();
+//     const y = h * Math.random();
+//     const r = Math.min(w, h) * (0.1 + 0.2 * Math.random());
+//     ctx.translate(x, y);
+//     flower(ctx, r);
+//     ctx.restore()
+// }
+
+// let total = 0;
+// do {
+//     total += Math.random();
+//     randomFlower(ctx);
+// } while (total < 10);
+
+//Providing more control
+
 import face from './face.js'
 
 const w = window.innerWidth;
@@ -199,38 +245,22 @@ canvas.width = w;
 canvas.height = h;
 
 const ctx = canvas.getContext('2d');
+const size = Math.min(w, h);
+ctx.translate((w - size)/2, (h - size)/2);
 
-function flower(ctx, radius) {
-    const r = radius / 4;
+const radius = size / 10;
+ctx.translate(radius, radius);
 
-    // One in the middle
-    face(ctx, r);
-    
-    // eight around the outside
-    for (const angle of [0, 1, 2, 3, 4, 5, 6, 7]) {
+for (const x of [0, 1, 2, 3, 4]) { 
+    for (const y of [0, 1, 2, 3, 4]) {
         ctx.save();
-        ctx.rotate(angle * Math.PI / 4);
-        ctx.translate(0, -r * 1.5);
-        face(ctx, r / 2);
-        ctx.restore()    
+        ctx.translate(2 * x * radius, 2 * y * radius);
+        face(ctx, radius, {
+            pupilOffset: { x: (2 - x) / 2, y: (2 - y) / 2 },
+            eyeSize: Math.random(),
+            eyeGap: Math.random(),
+
+        });
+        ctx.restore();
     }
 }
-
-function randomFlower(ctx) {
-    ctx.save();
-    const x = w * Math.random();
-    const y = h * Math.random();
-    const r = Math.min(w, h) * (0.1 + 0.2 * Math.random());
-    ctx.translate(x, y);
-    flower(ctx, r);
-    ctx.restore()
-}
-
-let total = 0;
-do {
-    total += Math.random();
-    randomFlower(ctx);
-} while (total < 10);
-
-
-
